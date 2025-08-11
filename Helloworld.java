2aff49c38b755e0491e027225dfdf65dc43cd729
“Gracecheng21” <cwy21graz@gmail.com>
2025-08-11 17:37:29 +0800

    test 11.08.2025

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    //means writing comment or note that no conflict with java
+
+    System.out.println("Hello");
+    //System.out.println("") text in "" to print out
+  }
+}
