2cbac3a40c1c75c3fab71a3f3504a5c79b3a09a1
“Gracecheng21” <cwy21graz@gmail.com>
2025-08-12 15:16:58 +0800

    test 12.08.25

--- Hi.java
+++ Hi.java
@@ -1,5 +1,7 @@
 public class Hi {
   public static void main(String[] args) {
     System.out.println("good try");
+    System.out.println("你好");
+    
   }
 }
---
+++ demo-javabasic/src/DemoDataType.java
@@ -0,0 +1,28 @@
+public class DemoDataType {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //Variable
+        int x = 3;
+        //put 7 into y
+        int y = 7;
+        System.out.println(x);
+        System.out.println("x");
+        //without "" printout the meaning of ()
+        //with "" printout directly what "" is
+        System.out.println(y);
+
+        //Re-assign value 10 into variable x
+        x = 10;
+        System.out.println(x);//x=10
+        //Re-assign y=100
+        y = 100;
+        System.out.println(y);
+        //shortcut: sysout > System.out.println()
+        System.out.println("y");
+
+
+        //Variable Declaration 命名變量
+        
+    }
+}
