8be0baeadc12088f1cb2ff96bd861ad57a07ffb8
“Gracecheng21” <cwy21graz@gmail.com>
2025-08-12 18:19:34 +0800

    exercise 12.08.25

--- demo-javabasic/src/DemoOperator.java
+++ demo-javabasic/src/DemoOperator.java
@@ -13,7 +13,25 @@ public class DemoOperator {
     int b = (y - 9) * 2;
     System.out.println(b); // 2
 
-    
+    //divided by zero error in java
+    // int r = 10 / 0; system error
 
+    int r2 = 0 / 10;
+    System.out.println(r2); // 0
+
+    // Java: int / int = int
+    int r3 = 10 / 3;
+    System.out.println(r3); // around to integer = 3
+    // int / int = int
+    System.out.println(5 / 2); // 2
+    // double / int = double / double = double
+    System.out.println(5.0 / 2); // 2.5
+
+    int score1 = 71;
+    int score2 = 82;
+    // code
+    double averageScore = (score1 + score2) / 2.0;
+    System.out.println(averageScore);
+    // 76.5 have to use double to calculate, double / double = double
   }
 }
