0d65ded6e40a7983f3d4f71f3ac1378ab042b971
“Gracecheng21” <cwy21graz@gmail.com>
2025-08-12 16:22:03 +0800

    datatypeNote 12.08.25

--- demo-javabasic/src/DemoDataType.java
+++ demo-javabasic/src/DemoDataType.java
@@ -2,7 +2,8 @@ public class DemoDataType {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        //Variable
+        //Variable 變量
+        // ! 1. interger 整數 - int
         int x = 3;
         //put 7 into y
         int y = 7;
@@ -22,7 +23,53 @@ public class DemoDataType {
         System.out.println("y");
 
 
-        //Variable Declaration 命名變量
+        // ! Declaration for variable type 聲名變量類型
+        int a;
+        //same variable name can be declared once only
+        //assignment
+        a = 4;
+        System.out.println(a);
         
+        // declaration and assignment
+        int w = 10;
+        System.out.println(w);
+
+        // ! double (small letter) 小數點
+        double d1 = 4.4;
+        double d2 = 3.14159;
+        System.out.println(d2);
+
+        double d3;
+        d3 = 1.2345678;
+        System.out.println(d3);
+
+        // ! +, -, *, /
+        int x2 = 6 + 1;
+        System.out.println(x2); // 7
+        int x3 = 9;
+
+        // ! put x3 value into variable x2
+        // = from right to left
+        x2 = x3;
+        System.out.println(x2); // 9
+        System.out.println(x3); // 9
+        x3 = x2;
+        System.out.println(x2); // 9
+        
+        int x4 = 10;
+        x2 = 7 + x4;
+        System.out.println(x2); // 6+1+10 = 17
+
+        // ! add to int itself
+        x2 = x2 + 8;
+        System.out.println(x2); // 6+1+10+8 = 25
+
+
+
+
+
+
+
+
     }
 }
